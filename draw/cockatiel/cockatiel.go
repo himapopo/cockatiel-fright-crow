cb8e6ac57a19b502d29155660c29c7ccc9bbc49f
himapopo <himapopo0507@yahoo.co.jp>
2023-10-04 01:33:49 +0900

    ゲームオーバー判定

--- draw/cockatiel/cockatiel.go
+++ draw/cockatiel/cockatiel.go
@@ -1,6 +1,7 @@
 package cockatiel
 
 import (
+	"cockatiel-fright-crow/update"
 	"image/png"
 	"log"
 	"os"
@@ -14,9 +15,17 @@ var (
 	// オカメ画像
 	okameImage *ebiten.Image
 
-	okameImageSizeX = 0.15
-	okameImageSizeY = 0.15
+	okameImageSizeX = 0.15 // 元の大きさ 745
+	okameImageSizeY = 0.15 // 元の大きさ 784
+
+	endPositionX float64 = 0
+	endPositionY float64 = 0
 )
+
+func ResetEndPosition() {
+	endPositionX = 0
+	endPositionY = 0
+}
 
 func Init() {
 	// オカメ画像ファイル
@@ -34,9 +43,25 @@ func Init() {
 	okameImage = ebiten.NewImageFromImage(i)
 }
 
-func ImageDraw(screen *ebiten.Image) {
+func ImageDraw(screen *ebiten.Image, state *update.GameState) {
+
+	var positionX float64 = 0
+	var positionY float64 = 0
 	// カーソルの位置
 	cursorX, cursorY := ebiten.CursorPosition()
+
+	if state.State == "end" && endPositionX == 0 && endPositionY == 0 {
+		endPositionX = float64(cursorX)
+		endPositionY = float64(cursorY)
+	}
+
+	if endPositionX != 0 && endPositionY != 0 {
+		positionX = endPositionX
+		positionY = endPositionY
+	} else {
+		positionX = float64(cursorX)
+		positionY = float64(cursorY)
+	}
 
 	oop := &ebiten.DrawImageOptions{}
 
@@ -44,7 +69,7 @@ func ImageDraw(screen *ebiten.Image) {
 	oop.GeoM.Scale(okameImageSizeX, okameImageSizeY)
 
 	// オカメ画像の位置
-	oop.GeoM.Translate(float64(cursorX), float64(cursorY))
+	oop.GeoM.Translate(positionX, positionY)
 
 	// オカメ画像描画
 	screen.DrawImage(okameImage, oop)
