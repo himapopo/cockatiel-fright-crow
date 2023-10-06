928d91576816c2f1d320d7f288560136b92a508d
himapopo <himapopo0507@yahoo.co.jp>
2023-10-07 02:03:31 +0900

    BGM, 効果音追加

--- update/state.go
+++ update/state.go
@@ -1,16 +1,42 @@
 package update
 
+import (
+	"bytes"
+	"log"
+
+	"github.com/ebitengine/oto/v3"
+	"github.com/hajimehoshi/ebiten/v2/audio/wav"
+	raudio "github.com/hajimehoshi/ebiten/v2/examples/resources/audio"
+)
+
+var (
+	AudioContext *oto.Context
+)
+
 type GameState struct {
 	Score int
 	State string
 	Level int
+
+	hitPlayer *oto.Player
 }
 
 func NewGameState() *GameState {
+
+	// 効果音初期化 TODO: 別の箇所に処理切り出す
+	jabD, err := wav.DecodeWithoutResampling(bytes.NewReader(raudio.Jab_wav))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	player := AudioContext.NewPlayer(jabD)
+
 	return &GameState{
 		Score: 0,
 		State: "start",
 		Level: 1,
+
+		hitPlayer: player,
 	}
 }
 
@@ -32,6 +58,9 @@ func (gs *GameState) GameRun() {
 
 func (gs *GameState) GameEnd() {
 	gs.State = "end"
+
+	gs.hitPlayer.Seek(0, 0)
+	gs.hitPlayer.Play()
 }
 
 func (gs *GameState) GameStart() {
