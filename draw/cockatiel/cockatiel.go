a32a54a52b466b38d4eda35c2cd56d66ef36e00a
yokokawa taiki <himapopo0507@yahoo.co.jp>
2023-10-08 04:09:53 +0900

    level調整。レアキャラ追加

--- draw/cockatiel/cockatiel.go
+++ draw/cockatiel/cockatiel.go
@@ -32,14 +32,6 @@ type Cockatiel struct {
 }
 
 func NewCockatiel() *Cockatiel {
-
-	// オカメ画像ファイル
-	// b, err := os.ReadFile("./assets/img/えんぴつオカメ.png")
-	// if err != nil {
-	// 	e := xerrors.Errorf("error: %w", err)
-	// 	log.Fatalf("%+v\n", e)
-	// }
-
 	i, err := png.Decode(bytes.NewReader(imageByte))
 	if err != nil {
 		e := xerrors.Errorf("error: %w", err)
@@ -51,29 +43,29 @@ func NewCockatiel() *Cockatiel {
 		image:       cockatielImage,
 		imageWidth:  0.08,
 		imageHeight: 0.08,
-		epx:         0,
-		epy:         0,
+		epx:         10000,
+		epy:         10000,
 		cpx:         0,
 		cpy:         0,
 	}
 }
 
 func (c *Cockatiel) Reset() {
-	c.epx = 0
-	c.epy = 0
+	c.epx = 10000
+	c.epy = 10000
 }
 
 func (c *Cockatiel) ImageDraw(screen *ebiten.Image, g *game.Game) {
 	c.cpx, c.cpy = CurrentPosition()
 
 	// ゲームオーバーした場所を保存
-	if g.State.State == "end" && c.epx == 0 && c.epy == 0 {
+	if g.State.State == "end" && c.epx == 10000 && c.epy == 10000 {
 		c.epx = c.cpx
 		c.epy = c.cpy
 	}
 
 	// ゲームオーバーした場所が登録されている場合は固定
-	if c.epx != 0 && c.epy != 0 {
+	if c.epx != 10000 && c.epy != 10000 {
 		c.cpx = c.epx
 		c.cpy = c.epy
 	}
